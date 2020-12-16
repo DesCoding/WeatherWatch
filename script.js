cdd544d475c0c4a9fb508773714ef43bb704f653
Des <d.stout31@yahoo.com>
2020-12-15 21:57:47 -0600

    updated JS script and linked to html

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ var latitude = "";
 var longitude = "";
 
 //API key from open weather 
-var APIKey = 9dfa6457815c2b95699d77e603110552
+var APIKey = "9dfa6457815c2b95699d77e603110552"
 
 var forecast
 
 //API call from open weather API
-api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
+
+$.ajax({
+    url: queryURL,
+    method: "GET"})
