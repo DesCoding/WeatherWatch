427f422220500b0871d8ae66e6aad4e4a1b35913
Des <d.stout31@yahoo.com>
2020-12-16 18:03:32 -0600

    declaring variables, adding some CSS

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 var city = "Atlanta";
 var latitude = "";
 var longitude = "";
+var now = moment();
+var savedCity = ["Atlanta"];
 
 //API key from open weather 
 var APIKey = "9dfa6457815c2b95699d77e603110552"
 
-var forecast
+var forecast = $("#five-day-forecast")
+var currentWeather = $("#current-weather")
 
 //API call from open weather API
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
