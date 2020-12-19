109be978993981ba16fbc2b664ca279058aa320f
Des <d.stout31@yahoo.com>
2020-12-18 18:15:12 -0600

    worked with tutor, updated JS, jQuery and added sample CSS to be updated

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var city = "Atlanta";
+var city = $("#user-input").val();
 var latitude = "";
 var longitude = "";
 //variable for moment.js 
@@ -10,7 +10,7 @@ var citiesArray = ["Atlanta"];
 var APIKey = "9dfa6457815c2b95699d77e603110552"
 
 var forecast = $("#five-day-forecast")
-var currentWeather = $("#current-weather")
+var currentWeatherDisplay = $("#current-weather")
 
 //Event listener and JQuery val method to return value of first matched element in the OpenWeatherAPI data
 $("#submit-btn").on("click", function(){
@@ -44,9 +44,9 @@ function saveCityToArray(object) {
     };
 };
 
-//API call from open weather API
+//API call from open weather API for current day
 function openWeatherAPIRequest() {
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
     //jQuery ajax method pulls info from the API
     $.ajax({
      url: queryURL,
@@ -57,10 +57,10 @@ function openWeatherAPIRequest() {
 
         saveCityToArray(response);
         $("#current-city-name").text(response.name);
-        $("#current-date").text(now.format("dddd mmmm do"));
+        $("#current-date").text(now.format("MMMM Do YYYY"));
     
         console.log("Currently saved cities:  ", citiesArray);
-        currentWeather.empty();
+        currentWeatherDisplay.empty();
         forecast.empty();
         latitude = response.coord.lat;
         longitude = response.coord.lon;
@@ -69,6 +69,56 @@ function openWeatherAPIRequest() {
     });
 
 };
+
+//API call from open weather API for five day
+function openWeatherAPIRequestFive() {
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
+    //jQuery ajax method pulls info from the API
+    $.ajax({
+     url: queryURL,
+     method: "GET"
+     //.then promises to retun the function (in this API) and captures the response
+    }).then(function (response) {
+        console.log("response: ", response);
+
+        //displayFiveDayForecast(response);
+        for (var i = 3; i < response.list.length; i = i+8){
+        displayFiveDayForecast(response.list[i], response.list[i].dt_txt)
+        }
+        // $("#current-city-name").text(response.name);
+        // $("#current-date").text(now.format("dddd mmmm do"));
+    
+        // console.log("Currently saved cities:  ", citiesArray);
+        // currentWeatherDisplay.empty();
+        // forecast.empty();
+        // latitude = response.coord.lat;
+        // longitude = response.coord.lon;
+
+        // coordinates (latitude, longitude);
+    });
+
+    
+
+};
+
+
+
+//Create buttons for city info
+function generateButtons(){
+
+    var btnGroup = $(".button-group");
+    btnGroup.empty();
+    displayStoredCities();
+
+    citiesArray.forEach(element => {
+        var cityBtn = $("<button type='button' class='city-btn btn btn-dark btn-lg btn-block'>");
+        cityBtn.text(element);
+        btnGroup.append(cityBtn);
+        console.log("cities: " + element);
+        console.log(cityBtn);
+    });
+};
+
 
 function coordinates(latitude, longitude) {
     var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIKey;
@@ -81,14 +131,94 @@ function coordinates(latitude, longitude) {
 
         currentWeather(response.current);
         console.log(response);
+        //for loop to find five day forecast
+        // for (i = 1; i < 6; i++){
 
-        for (i = )
+        //     console.log("five day: ", response.daily[i]);
+        //     displayFiveDayForecast(response.daily[i], now.add(i, 'days').foremat("dddd"));
+        // };
 
-    })
+        generateButtons();
 
+        $(".city-btn").on("click", function () {
+            console.log("clicked");
+            city = $(this).text();
+            console.log("clicked city: " + this.text())
 
+            openWeatherAPIRequest();
+            openWeatherAPIRequestFive();
+        });
 
+    });
+};
 
+function currentWeather(current) {
+var iconcode = current.weather[0].icon;
+var iconURL = "https://openweathermap.org/img/w/" + iconcode + ".png";
+$("#current-icon").attr("src", iconURL);
 
+var temp = Math.floor((current.temp - 273.15) * 1.80 + 32);
 
+var currentTemp = $("<h4>").text("Temperature: " + temp + " °F");
+var currentHumidity = $("<h4>").text("Humidity: " + current.humidity + "%");
+var currentWind = $("<h4>").text("Wind Speed: " + current.wind_speed + " MPH");
+var currentUV = $("<h4 id='uvi'> ").text("UV Index: ");
+
+if (current.uvi <= 2) {
+    currentUV.append("<span id='uvi-low'>" + current.uvi);
+} else if (current.uvi > 2 && current.uvi <= 7) {
+    currentUV.append("<span id='uvi-mid'>" + current.uvi);
+}else if ( current.uvi > 7){
+    currentUV.append("<span id='uvi-high'>" + current.uvi);
 }
+
+currentWeatherDisplay.append(currentTemp, currentHumidity, currentWind, currentUV);
+console.log(current);
+};
+
+
+
+function displayFiveDayForecast(daily, date) {
+console.log("here?")
+console.log("daily", daily)
+var temp = Math.floor((daily.main.temp - 273.15) * 1.80 + 32);
+
+console.log(temp)
+var card = $("<div class='card daily-forecast'>");
+//Create card div and adding date parameter
+var cardBody = $("<div class='card-body'>");
+var cardTitle = $("<h3 class='card-title text-center'>").text(date);
+
+var iconcode = daily.weather[0].icon;
+var iconURL = "https://openweathermap.org/img/w/" + iconcode + ".png";
+var icon = $("<img class='icons' src=" + iconURL + " alt='Weather icon'>");
+
+var futureTemp = $("<h5>").text("Temp:  " + temp + " °F");
+var futureHumidity = $("<h5>").text("Humidity: " + daily.humidity + "%");
+
+cardBody.append(cardTitle, $("<hr>"), icon, futureTemp, futureHumidity);
+card.append(cardBody);
+forecast.append(card);
+};
+
+
+function storeCities() {
+localStorage.setItem("savedCities", JSON.stringify(citiesArray));
+};
+
+function displayStoredCities() {
+
+var storedCities = JSON.parse(localStorage.getItem("savedCities"));
+
+if (storedCities != null) {
+    citiesArray = storedCities;
+};
+};
+
+openWeatherAPIRequest();
+openWeatherAPIRequestFive();
+
+
+
+
+
